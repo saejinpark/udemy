74a6e52ba2eec77edbb543e9fb714578d1ef9a30
saejinpark <saejinpark951210@gmail.com>
2022-11-29 22:20:43 +0900

    일일커밋

--- 【한글자막】 The Web Developer 부트캠프 2022/YelpCamp/seeds/index.js
+++ 【한글자막】 The Web Developer 부트캠프 2022/YelpCamp/seeds/index.js
@@ -20,9 +20,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${ciries[random1000].city} ${ciries[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image:
+        "https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita magni velit pariatur nobis reprehenderit deserunt eos in impedit delectus iure eaque dignissimos enim repudiandae odit ab aut, accusamus obcaecati quidem.",
+      price,
     });
     await camp.save();
   }
